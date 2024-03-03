c563e5837c2220f9955359bdcfc911c95e546c31
Khan Tarique <97331285+khantariqu@users.noreply.github.com>
2024-03-04 03:31:28 +0530

    Update MavenApplicationCrudApplication.java

--- src/main/java/com/javatechie/mavenapplicationcrud/MavenApplicationCrudApplication.java
+++ src/main/java/com/javatechie/mavenapplicationcrud/MavenApplicationCrudApplication.java
@@ -12,6 +12,7 @@ public class MavenApplicationCrudApplication {
 		System.out.println("updated 1 by tarique");
 		System.out.println("updated 2 by tarique");
 		System.out.println("updated 2 by tarique");
+		System.out.println("updated 3 by tarique");
 		
 	}
 
